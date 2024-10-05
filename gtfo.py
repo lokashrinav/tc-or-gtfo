d64da7750b4efa2746c6acf41d68e5da093f45d7
Shrinav Loka <lokashrinav@gmail.com>
2024-10-05 04:05:01 -0400

    FINISHED TC-OR-GTFO BOT. Am planning to create this for non-sensitive content in the future using OpenAI API

--- gtfo.py
+++ gtfo.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 driver = webdriver.Chrome()
 
@@ -16,11 +18,11 @@ time.sleep(5)
 
 username = driver.find_elements(By.CSS_SELECTOR, '#email')[0]
 
-username.send_keys("shrinav@kashmirworldfoundation.org")
+username.send_keys("EMAIL-REPLACE")
 
 password = driver.find_elements(By.CSS_SELECTOR, '#password')[0]
 
-password.send_keys("Jaguar2005@8811")
+password.send_keys("PASSWORD-REPLACE")
 
 sign_in2 = driver.find_elements(By.CSS_SELECTOR, '.inline-flex.items-center.justify-center.rounded-lg.text-sm.font-semibold.transition-colors.text-white.h-10.px-4.bg-black.hover\\:bg-gray-999.active\\:bg-black.w-full.disabled\\:bg-gray-200.disabled\\:text-gray-999')[0]
 
@@ -41,15 +43,23 @@ for link in list1:
     print(link)
     driver.execute_script("window.location.href = arguments[0];", link)    
     time.sleep(3)
-    text = driver.find_element(By.CSS_SELECTOR, '.whitespace-pre-wrap.break-words.text-sm\\/5.text-gray-999\\[word-break\\:break-word\\].md\\:text-base\\/6').text
-    if 'tc' in text.lower():
-        print("Hello World")
-    else:
-        print("TC or GTFO")
+    
+    text = driver.find_element(By.CSS_SELECTOR, '.break-words').text
+    if 'tc' not in text.lower():
+        text = driver.find_element(By.CSS_SELECTOR, 'textarea')
+        text.send_keys("TC or GTFO")
+        
+        buttons = driver.find_elements(By.CSS_SELECTOR, '.inline-flex.items-center.justify-center.text-sm.font-semibold.transition-colors.bg-black')
+        
+        time.sleep(5)
+        
+        for elem in buttons:
+            if elem.text and elem.text == "Post":                                
+                elem.click()
+
+                
     driver.back()
         
-time.sleep(5)
-
 
 
 
